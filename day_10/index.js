87855a1b0d8d3c8a0c157b6b4a5c0b32a252d258
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2024-12-10 20:05:00 +0000

    part one and part two

--- day_10/index.js
+++ day_10/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("test.txt", "utf8", (err, data) => {
+fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err);
     return;
@@ -12,35 +12,72 @@ fs.readFile("test.txt", "utf8", (err, data) => {
   getPartOne(numericInput);
 });
 
+let partOneCounter = 0;
+let partTwoCounter = 0;
+let nineLocations = [];
+
 const getPartOne = (input) => {
   input.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell === 0) {
-        console.log("Found 0 at", x, y);
+        nineLocations = [];
         searchGraph(input, cell + 1, x, y);
       }
     });
   });
+
+  console.log("Part one:", partOneCounter);
+  console.log("Part two:", partTwoCounter);
 };
 
 const searchGraph = (input, target, x, y) => {
-  console.log("Searching for", target, "at", x, y);
-  const neighbours = getNeighbours(input, Number(target), x, y);
-  console.log("Neighbours", neighbours);
+  if (target === 10) return;
+
+  const neighbours = geValidNeighbours(input, Number(target), x, y);
+
+  if (neighbours.length === 0) {
+    return;
+  }
+
+  neighbours.forEach((neighbour) => {
+    searchGraph(input, target + 1, neighbour.x, neighbour.y);
+  });
+
+  if (target === 9 && neighbours.length !== 0) {
+    neighbours.forEach((neighbour) => {
+      partTwoCounter++;
+      const hasBeenVisited = nineLocations.some(
+        (location) => location.x === neighbour.x && location.y === neighbour.y
+      );
+
+      if (!hasBeenVisited) {
+        nineLocations.push({ x: neighbour.x, y: neighbour.y });
+        partOneCounter++;
+      }
+    });
+  }
 };
 
-const getNeighbours = (input, target, x, y) => {
+const geValidNeighbours = (input, target, x, y) => {
   const neighbours = [];
 
   if (input[y - 1]?.[x] === target) {
     neighbours.push({ x, y: y - 1 });
-  } else if (input[y + 1]?.[x] === target) {
+  }
+
+  if (input[y + 1]?.[x] === target) {
     neighbours.push({ x, y: y + 1 });
-  } else if (input[y]?.[x - 1] === target) {
+  }
+
+  if (input[y]?.[x - 1] === target) {
     neighbours.push({ x: x - 1, y });
-  } else if (input[y]?.[x + 1] === target) {
+  }
+
+  if (input[y]?.[x + 1] === target) {
     neighbours.push({ x: x + 1, y });
-  } else {
+  }
+
+  if (neighbours.length === 0) {
     return [];
   }
 
