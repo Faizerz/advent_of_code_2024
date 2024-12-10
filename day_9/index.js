3e491b789cf6173fbd83b2e88d11598242186c88
fez <fez@thoughtmachine.net>
2024-12-10 13:37:13 +0000

    why is this not correct

--- day_9/index.js
+++ day_9/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const isDivisibleByTwo = (number) => Number(number) % 2 === 0;
 
-fs.readFile("test.txt", "utf8", (err, data) => {
+fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err);
     return;
@@ -11,34 +11,36 @@ fs.readFile("test.txt", "utf8", (err, data) => {
   const inputNumbers = data.split("");
 
   let counter = 0;
-  let dotCounter = 0;
-  let justNumbers = [];
 
   const fullString = inputNumbers.reduce((acc, curr, i) => {
     if (isDivisibleByTwo(i) && curr !== ".") {
-      const repeated = `${counter++}`.repeat(Number(curr));
-      justNumbers.push(...repeated);
-      return acc + repeated;
+      return acc + `${counter++}`.repeat(Number(curr));
     } else {
-      dotCounter += Number(curr);
       return acc + ".".repeat(curr);
     }
   }, "");
 
-  let currentString = fullString.split("");
-  let numbersToInsert = justNumbers.reverse();
-  let dotCount = 0;
+  // console.log(fullString);
 
-  currentString.forEach((char, i) => {
-    if (char === "." && dotCount < dotCounter) {
-      dotCount++;
-      currentString[i] = numbersToInsert.shift();
+  let currentString = fullString.split("");
+
+  fullString.split("").forEach((char, i) => {
+    if (char === ".") {
+      for (let j = currentString.length - 1; j > i; j--) {
+        if (currentString[j] !== ".") {
+          currentString[i] = currentString[j];
+          currentString[j] = ".";
+          break;
+        }
+      }
+      // console.log(currentString.join(""));
     }
   });
 
-  const answer = currentString
-    .slice(0, currentString.length - dotCounter) // Use currentString.length here
-    .reduce((acc, curr, i) => (curr !== "." ? acc + Number(curr) * i : acc), 0);
+  const answer = currentString.reduce(
+    (acc, curr, i) => (curr !== "." ? acc + Number(curr) * i : acc),
+    0
+  );
 
   console.log(answer);
 });
