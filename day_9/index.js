ccd1578c3c706f4c79cfcd752d54c799f6a288c7
fez <fez@thoughtmachine.net>
2024-12-10 17:06:33 +0000

    It gives the answer... but at what cpu cost

--- day_9/index.js
+++ day_9/index.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 
-const isDivisibleByTwo = (number) => Number(number) % 2 === 0;
-
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err);
     return;
   }
 
-  const fullString = data.split("").reduce((acc, curr, i) => {
+  // Part One
+  const partOnefullString = data.split("").reduce((acc, curr, i) => {
     if (isDivisibleByTwo(i) && curr !== ".") {
       return [...acc, ...Array(Number(curr)).fill(i / 2)];
     } else {
@@ -16,7 +15,18 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     }
   }, []);
 
-  console.log(getPartOne(fullString));
+  console.log(getPartOne(partOnefullString));
+
+  // Part Two
+  const partTwoFullString = data.split("").reduce((acc, curr, i) => {
+    if (isDivisibleByTwo(i) && curr !== ".") {
+      return [...acc, Array(Number(curr)).fill(i / 2)];
+    } else {
+      return [...acc, Array(Number(curr)).fill(".")];
+    }
+  }, []);
+
+  console.log(getPartTwo(partTwoFullString));
 });
 
 const getPartOne = (fullString) => {
@@ -36,4 +46,63 @@ const getPartOne = (fullString) => {
     (acc, curr, i) => (curr !== "." ? acc + Number(curr) * i : acc),
     0
   );
+};
+
+const getPartTwo = (fullString) => {
+  let hasChanges = true;
+
+  while (hasChanges) {
+    hasChanges = processFullString(fullString);
+    fullString = fullString.filter((subArray) => subArray.length > 0);
+  }
+
+  return fullString
+    .flat()
+    .reduce((acc, curr, i) => (curr !== "." ? acc + Number(curr) * i : acc), 0);
+};
+
+const isDivisibleByTwo = (number) => Number(number) % 2 === 0;
+
+const processFullString = (fullString) => {
+  let hasChanges = false;
+
+  for (let i = 0; i < fullString.length; i++) {
+    for (let j = fullString.length - 1; j > i; j--) {
+      if (
+        fullString[i][0] === "." &&
+        fullString[j][0] !== "." &&
+        fullString[i].length >= fullString[j].length
+      ) {
+        for (let x = 0; x < fullString[i].length; x++) {
+          if (fullString[j][x] !== undefined) {
+            fullString[i][x] = fullString[j][x];
+            fullString[j][x] = ".";
+          } else {
+            const split = splitArray(fullString[i]);
+            fullString.splice(i, 1, split[0], split[1]);
+
+            hasChanges = true;
+            return true;
+          }
+        }
+        hasChanges = true;
+      }
+    }
+  }
+
+  return hasChanges;
+};
+
+const splitArray = (array) => {
+  const result = [[], []];
+
+  for (const item of array) {
+    if (item === ".") {
+      result[1].push(item);
+    } else {
+      result[0].push(item);
+    }
+  }
+
+  return result;
 };
